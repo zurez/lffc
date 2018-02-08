89975c07fdd75d9aeff5e611838175ba4e4f6e76
zurez <zurez4u@gmail.com>
2018-02-08 13:22:27 +0530

    ff

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -10,17 +10,33 @@ use App\Customers;
 use App\Jobs\SendFirebaseNotifications;
 class FirebaseController extends Controller
 {
+
+    public function show_notify_form()
+    {
+        
+    }
     public function notify()
 {	        $tokens=[
                 "cuRZqiI-rjw:APA91bFItALnt2LuK5o76chTPPmW-DbgdbM5qxrU031_UPpbOxnH5YRXsoKZsHA_UqceE8zj2cAqVAye9CTT7dCCtniiXNTKL64VEsihVrBDJApughc6iEdlfHqnFDw0yIErKINGZ7sq",
                 "e5mrjEIpnzM:APA91bFLKe4-ibTErh-S4u-IJuIwAG35yK7pIrKpvyHE3pdfFbt1MmN6L7yMKfNzymIM8wYWX0sO-JFrWdS75yf3ViogsAiyt1_0fjh43JQjFeBvT5nfSVaTdg2Oan99qHpFh8PsMa3t"
 
             ];
+
         $delay_time=0;
+        $flag=0;
+        $limit=100;
         for ($i=0; $i <4; $i++) { 
-        	dump($delay_time);
+        	$customers=Customers::where("ftoken","!=","")->skip($flag)->take($limit)->get();
+
+            $flag+=$limit;
+            $tokens=array();
+
+            foreach ($customers as $f) {
+                array_push($tokens,$f->ftoken);
+            }
+            dump($tokens);
         	$job= (new SendFirebaseNotifications($tokens))->delay($delay_time);
-        	$delay_time+=60;
+        	$delay_time+=600;
         	$this->dispatch($job);
 
         }
